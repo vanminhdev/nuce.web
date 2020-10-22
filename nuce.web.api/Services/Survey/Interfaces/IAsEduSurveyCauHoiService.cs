cff72e53a74221d2f375e2c24fde70a626d21be1
Lê Văn Minh <vanminh.dev@gmail.com>
2020-10-22 11:40:39 +0700

    edit context

---
+++ nuce.web.api/Services/Survey/Interfaces/IAsEduSurveyCauHoiService.cs
@@ -0,0 +1,13 @@
+﻿using nuce.web.api.Models.Survey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nuce.web.api.Services.Survey.Interfaces
+{
+    public interface IAsEduSurveyCauHoiService
+    {
+        public Task<List<AsEduSurveyCauHoi>> GetAll();
+    }
+}
