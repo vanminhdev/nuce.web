1c3604a51062e248e46fb2e71ca2fd963f516241
nghia.nguyen <nghiant9068@gmail.com>
2020-10-29 10:27:18 +0700

    Thêm bảng Log

---
+++ nuce.web.api/Models/Core/ActivityLogs.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace nuce.web.api.Models.Core
+{
+    public partial class ActiviyLogs
+    {
+        public long Id { get; set; }
+        public long? UserId { get; set; }
+        public string UserCode { get; set; }
+        public string RoleId { get; set; }
+        public int? Status { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public DateTime? CreatedTime { get; set; }
+    }
+}
